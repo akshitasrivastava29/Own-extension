6b1fad93038e900558c471e7f9ed2fff02137be8
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-07-01 20:43:18 +0530

    Add files via upload
    Create own extension.

---
+++ Extension/index.js
@@ -0,0 +1,11 @@
+const getJokes=async()=>{
+    try{
+        const res= await fetch("https://api.chucknorris.io/jokes/random");
+        const data=await res.json();
+        const myjoke=document.querySelector("#myjoke");
+        myjoke.innerHTML=data.value;
+    }catch(error){}
+};
+window.addEventListener("load",()=>{
+    getJokes();
+});
